b25ad70dc673e3bc5730c44aa96c20c0826ca368
derrickb3 <44348986+thecodeweaver@users.noreply.github.com>
2024-09-17 16:25:52 -0400

    Added password generation functionality

--- password_generation.js
+++ password_generation.js
@@ -6,5 +6,26 @@ function generatePassword()
     positions[0] = positions[0] % wordlist.length;
     positions[1] = positions[1] % wordlist.length;
 
-    console.log(positions);
+    // Get special character to be used in password
+    special_chars = ['!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
+    special_char_position = new Uint8Array(1);
+    self.crypto.getRandomValues(special_char_position);
+    special_char_position[0] = special_char_position[0] % special_chars.length;
+
+    // Generate number to be used in password
+    number = new Uint8Array(1);
+    self.crypto.getRandomValues(number);
+    number[0] = number[0] % 10;
+
+    // Create password string
+    password = "";
+    // Capitalize first letter of each word
+    password += wordlist[positions[0]][0].toLocaleUpperCase() + wordlist[positions[0]].slice(1);
+    password += wordlist[positions[1]][0].toLocaleUpperCase() + wordlist[positions[1]].slice(1);
+
+    password += number[0];
+    password += special_chars[special_char_position[0]];
+    
+    document.getElementById("passwordOutput").value = password;
 }
